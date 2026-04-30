f2a1e32aecd31dea0a0af6f84c106e397bbbbe85
ganesanus14 <ganesanus14@gmail.com>
2026-04-30 19:59:10 -0400

    Add project for review

---
+++ runing.cmd
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Add project for review"
+git branch -M main
+git remote add origin https://github.com/ganesanus14/DuplicatePhotoFinder.git
+git push -u origin main
